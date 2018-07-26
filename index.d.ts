bc0fcbcfc8f97e03d5990a1d854d4ae43a110bf1
Sami Jaber <jabersami@gmail.com>
2018-07-25 21:38:23 -0400

    Add TypeScript declaration file (#90)

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+declare module 'extract-css-chunks-webpack-plugin' {
+  import { Loader, Plugin } from 'webpack';
+
+  class ExtractCssChunksPlugin extends Plugin {
+      static loader: Loader;
+
+      constructor(options?: ExtractCssChunksPlugin.PluginOptions);
+  }
+
+  namespace ExtractCssChunksPlugin {
+      interface PluginOptions {
+          /**
+           * Options similar to those of `mini-css-extract-plugin`
+           */
+          filename?: string;
+          chunkFilename?: string;
+          /**
+           * we try to automatically inject hot reloading, but if it's not working, use this config
+           */
+          hot?: boolean;
+      }
+  }
+
+  export = ExtractCssChunksPlugin;
+}
