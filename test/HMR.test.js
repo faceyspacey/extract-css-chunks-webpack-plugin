fdc5e7be45a15fee356b2d0a7d1cf25187bb9295
Maarten Van Hoof <info@mrtnvh.com>
2020-10-15 05:21:43 +0200

    feat: preload css (#300)
    * WIP - preload CSS
    
    Inspired by https://github.com/webpack-contrib/mini-css-extract-plugin/pull/344
    
    * feat: preload css
    
    * test(TestCases): re-enable logging

--- test/HMR.test.js
+++ test/HMR.test.js
@@ -30,7 +30,8 @@ describe('HMR', () => {
 
     jest.spyOn(Date, 'now').mockImplementation(() => 1479427200000);
 
-    document.head.innerHTML = '<link rel="stylesheet" href="/dist/main.css" />';
+    document.head.innerHTML =
+      '<link rel="preload" as="style" href="/dist/main.css" />';
     document.body.innerHTML = '<script src="/dist/main.js"></script>';
   });
 
--- test/cases/insert-function/webpack.config.e2e.js
+++ test/cases/insert-function/webpack.config.e2e.js
@@ -64,7 +64,9 @@ module.exports = {
     new Self({
       filename: '[name].css',
       chunkFilename: '[id].css',
-      insert: 'body',
+      insert: (linkTag) => {
+        document.head.appendChild(linkTag)
+      },
     }),
   ],
   devServer: {
